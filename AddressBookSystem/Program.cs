2e63da1e3ba35154e299ea9733ebfceee8c7a1aa
rani1029 <ranig1029@gmail.com>
2021-09-11 13:40:15 +0530

    Contact added

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,6 +7,36 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address Book Management system");
+
+            Person personinfo = new Person();
+
+            Console.WriteLine(" add contacts");
+
+            Console.WriteLine(" enter FirstName");
+            personinfo.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            personinfo.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address : ");
+            personinfo.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City Name : ");
+            personinfo.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State Name : ");
+            personinfo.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip-Code : ");
+            personinfo.ZipCode = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number : ");
+            personinfo.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email-ID : ");
+            personinfo.EmailId = Console.ReadLine();
+
+
         }
     }
 }
